f6da7284560cfa3c5366af23ad06adc7b5ce73e2
Kaoticz <1812311+Kaoticz@users.noreply.github.com>
2022-06-09 17:05:35 -0300

    Corrigir escopo das variáveis

--- IMC/Program.cs
+++ IMC/Program.cs
@@ -1,11 +1,16 @@
-
 Console.Write("Digite sua altura em metros: ");
 var altura = double.Parse(Console.ReadLine()!);
 
 Console.Write("Digite seu peso em KG: ");
 var peso = double.Parse(Console.ReadLine()!);
 
-string RetornarDiagnosticoDoUsuario(double alturaDoUsuario, double pesoDoUsuario)
+// Utilize a mensagem a seguir como base do retorno esperado:
+// Seu diagnóstico é XXXXXX.
+Console.WriteLine($"Seu diagnóstico é {RetornarDiagnosticoDoUsuario(altura, peso)}");
+
+// .::Funções::.
+
+static string RetornarDiagnosticoDoUsuario(double altura, double peso)
 {
     var imc = peso / (altura * altura);
 
@@ -19,7 +24,3 @@ string RetornarDiagnosticoDoUsuario(double alturaDoUsuario, double pesoDoUsuario
         _ => "Valor não faz sentido"
     };
 }
-
-// Utilize a mensagem a seguir como base do retorno esperado:
-// Seu diagnóstico é XXXXXX.
-Console.WriteLine($"Seu diagnóstico é {RetornarDiagnosticoDoUsuario(altura, peso)}");
