ff23598ec0358dc7d923a56f3aacef478cbde6a7
Kaoticz <1812311+Kaoticz@users.noreply.github.com>
2022-06-09 15:52:04 -0300

    Initial Commit

---
+++ ConversaoRealDolar/Program.cs
@@ -0,0 +1,15 @@
+// Cotação do dólar no dia 3 de maio de 2022.
+double cotacaoDoDolarHoje = 4.97;
+const double VALOR_MINIMO_DE_COMPRA_EM_DOLAR = 100;
+const double VALOR_MAXIMO_DE_COMPRA_EM_DOLAR = 2500;
+
+string converterRealParaDolar(double valorDaCompraEmReais)
+{
+
+}
+
+// Utilize as mensagens a seguir como base do retorno esperado:
+// A compra mínima permitida é de $ XXX,XX.
+// A compra máxima permitida é de $ XXXX,XX.
+// Você pode comprar $ XXX,XX.
+Console.WriteLine(converterRealParaDolar());
